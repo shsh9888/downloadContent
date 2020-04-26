5ad31746351525b685c48244bce35a3d4d218f4e
Shravan Shetty <shsh9888@colorado.edu>
2020-04-25 23:37:20 -0600

    added image downloader logic

--- app/src/main/java/com/example/webcontent/MainActivity.java
+++ app/src/main/java/com/example/webcontent/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.webcontent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    ImageView downloadedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        downloadedImage = (ImageView) findViewById(R.id.imageView);
 
         String result = null;
         DownloadTask downloadTask = new DownloadTask();
@@ -24,6 +28,22 @@ public class MainActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void downloadImage(View view) {
+        Log.i("Interaction", "Button tapped");
+        //https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png
+
+        ImageDownloader imageDownloader = new ImageDownloader();
+        Bitmap myImage;
+
+        try {
+            myImage = imageDownloader.
+                    execute("https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png").get();
+            downloadedImage.setImageBitmap(myImage);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
